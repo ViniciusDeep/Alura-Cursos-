53c56405faef1eea89f20bc638aa425824e4445b
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-09-07 19:12:15 -0300

    Create functions JS

--- JS/introducao-javascript/js/form.js
+++ JS/introducao-javascript/js/form.js
@@ -6,26 +6,11 @@ function setPaciente(event) {
 	var form = document.querySelector("#form-add");
 
 	var paciente = obtemInfo(form);
+	var pacienteTr = montaTr(paciente);
+
 	console.log(paciente);
 
-	var pacienteTr = document.createElement("tr");
-	var nomeTd = document.createElement("td");
-	var pesoTd = document.createElement("td");
-	var alturaTd = document.createElement("td");
-	var gorduraTd = document.createElement("td");
-	var imcTd = document.createElement("td");
 
-	nomeTd.textContent = nome;
-	pesoTd.textContent = peso;
-	alturaTd.textContent = altura;
-	gorduraTd.textContent = gordura;
-	imcTd.textContent = imc;
-	
-	pacienteTr.appendChild(nomeTd);
-	pacienteTr.appendChild(pesoTd);
-	pacienteTr.appendChild(alturaTd);
-	pacienteTr.appendChild(gorduraTd);
-	pacienteTr.appendChild(imcTd);
 
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
@@ -42,4 +27,23 @@ function obtemInfo(form) {
      	imc: calculaImc(form.peso.value, form.altura.value)
      }
      return paciente;
+}
+
+function montaTr(paciente) {
+	var pacienteTr = document.createElement("tr");
+	
+	pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+	pacienteTr.appendChild(montaTd(paciente.peso,"info-peso"));
+	pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+	pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+	pacienteTr.appendChild(montaTd(paciente.imc,"info-imc"));
+
+	return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+	var td = document.createElement("td");
+	td.textContent = dado;
+	td.classList.add(classe);
+	return td;
 }
