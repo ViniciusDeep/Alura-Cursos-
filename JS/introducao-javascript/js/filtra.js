a00604755474a74127ed828bfee66dad6e983ffb
Victor <victor@MacBook-Pro-de-Victor.local>
2018-09-08 20:47:56 -0300

    Function filter

--- JS/introducao-javascript/js/filtra.js
+++ JS/introducao-javascript/js/filtra.js
@@ -1,5 +1,10 @@
 var text = document.querySelector("#filtrar-tabela");
 
-text.addEventListener("click", function(){
-    console.log(text.value);
+text.addEventListener("input", function(){
+    var pacientes = document.querySelectorAll(".paciente");
+    for(var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var nomeTd = paciente.querySelector("info-nome");
+      var nome = nomeTd.textContent;
+    }
 });
