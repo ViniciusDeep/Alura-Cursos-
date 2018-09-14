f166a8e7456e21298d71d0ab4a3a2cecb2385275
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2018-09-14 15:15:16 -0300

    Class 3 ex 2

---
+++ JS/aluraframe/client/app/controllers/NegociacaoController.js
@@ -0,0 +1,21 @@
+class NegociacaoController {
+
+
+    adciona(event) {
+      event.preventDefault();
+      let data = document.querySelector("#data");
+      let quantidade = document.querySelector("#quantidade");
+      let valor = document.querySelector("#valor");
+
+      console.log(valor.value);
+      console.log(quantidade.value);
+      console.log(data.value);
+
+
+
+
+    }
+
+
+
+}
